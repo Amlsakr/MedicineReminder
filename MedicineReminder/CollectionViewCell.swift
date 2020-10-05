9e1bd6f2a3dc94786d724c55ae7f41fc6dd1e5d5
Aml <amlsakr@192.168.183.128>
2020-10-05 07:15:46 -0700

    collection view cell design

---
+++ MedicineReminder/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  MedicineReminder
+//
+//  Created by Aml Sakr on ٥/١٠/٢٠٢٠.
+//  Copyright © 2020 Aml Sakr. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dayNumberLabel: UILabel!
+    
+    @IBOutlet weak var dayNameLabel: UILabel!
+}
